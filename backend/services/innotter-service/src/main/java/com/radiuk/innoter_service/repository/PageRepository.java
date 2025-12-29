33638e6cbe45771d183fb70a2869a48cdc3a77d9
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2025-12-29 13:18:09 +0300

    add repository layer

---
+++ backend/services/innotter-service/src/main/java/com/radiuk/innoter_service/repository/PageRepository.java
@@ -0,0 +1,10 @@
+package com.radiuk.innoter_service.repository;
+
+import com.radiuk.innoter_service.entity.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PageRepository extends JpaRepository<Page, Long> {
+
+}
