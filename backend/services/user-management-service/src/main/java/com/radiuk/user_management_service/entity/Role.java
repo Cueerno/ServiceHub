9a8a9c1202108418dfeadeda62245da37cd04f06
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2025-12-30 02:01:46 +0300

    add entities

---
+++ backend/services/user-management-service/src/main/java/com/radiuk/user_management_service/entity/Role.java
@@ -0,0 +1,8 @@
+package com.radiuk.user_management_service.entity;
+
+public enum Role {
+
+    USER,
+    ADMIN,
+    MODERATOR
+}
---
+++ backend/services/user-management-service/src/main/java/com/radiuk/user_management_service/entity/User.java
@@ -0,0 +1,63 @@
+package com.radiuk.user_management_service.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String firstname;
+
+    @Column(nullable = false, length = 100)
+    private String lastname;
+
+    @Column(nullable = false, length = 64)
+    private String password;
+
+    @Column(nullable = false, length = 25)
+    private String phoneNumber;
+
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 50)
+    private Role role;
+
+    @Column(length = Integer.MAX_VALUE)
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    @Column(name = "is_blocked", nullable = false)
+    private Boolean isBlocked = false;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+
+}
