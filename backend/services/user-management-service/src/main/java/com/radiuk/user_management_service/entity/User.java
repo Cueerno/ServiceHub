555e3dffd425027131a1df05171ffc3623e507aa
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2025-12-31 17:23:20 +0300

    add refresh token association to User entity

--- backend/services/user-management-service/src/main/java/com/radiuk/user_management_service/entity/User.java
+++ backend/services/user-management-service/src/main/java/com/radiuk/user_management_service/entity/User.java
@@ -7,6 +7,8 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -63,4 +65,6 @@ public class User {
     @Column(name = "updated_at", nullable = false)
     private Instant updatedAt;
 
+    @OneToMany(mappedBy = "user", cascade =  CascadeType.ALL, orphanRemoval = true)
+    List<RefreshToken> refreshTokens = new ArrayList<>();
 }
