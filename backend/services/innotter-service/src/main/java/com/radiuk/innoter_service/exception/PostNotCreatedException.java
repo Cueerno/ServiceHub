d3290367e1cf8f65214e53449f2b2295aa033930
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2026-01-09 23:30:08 +0300

    add custom exceptions

---
+++ backend/services/innotter-service/src/main/java/com/radiuk/innoter_service/exception/PostNotCreatedException.java
@@ -0,0 +1,7 @@
+package com.radiuk.innoter_service.exception;
+
+public class PostNotCreatedException extends RuntimeException {
+    public PostNotCreatedException(String message) {
+        super(message);
+    }
+}
---
+++ backend/services/innotter-service/src/main/java/com/radiuk/innoter_service/exception/TagNotCreatedException.java
@@ -0,0 +1,7 @@
+package com.radiuk.innoter_service.exception;
+
+public class TagNotCreatedException extends RuntimeException {
+    public TagNotCreatedException(String message) {
+        super(message);
+    }
+}
