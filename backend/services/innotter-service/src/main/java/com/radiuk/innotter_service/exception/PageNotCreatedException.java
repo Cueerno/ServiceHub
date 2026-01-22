964095a49960630911992e5305e44b01dfd467e3
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2026-01-22 23:55:52 +0300

    refactor: improve custom exceptions, add constructors as in RuntimeException.class

--- backend/services/innotter-service/src/main/java/com/radiuk/innotter_service/exception/PageNotCreatedException.java
+++ backend/services/innotter-service/src/main/java/com/radiuk/innotter_service/exception/PageNotCreatedException.java
@@ -1,7 +1,24 @@
 package com.radiuk.innotter_service.exception;
 
 public class PageNotCreatedException extends RuntimeException {
+
+    public PageNotCreatedException() {
+        super();
+    }
+
     public PageNotCreatedException(String message) {
         super(message);
+    }
+
+    public PageNotCreatedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PageNotCreatedException(Throwable cause) {
+        super(cause);
+    }
+
+    protected PageNotCreatedException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
--- backend/services/user-management-service/src/main/java/com/radiuk/user_management_service/exception/UserNotUpdatedException.java
+++ backend/services/user-management-service/src/main/java/com/radiuk/user_management_service/exception/UserNotUpdatedException.java
@@ -1,7 +1,24 @@
 package com.radiuk.user_management_service.exception;
 
 public class UserNotUpdatedException extends RuntimeException {
+
+    public UserNotUpdatedException() {
+        super();
+    }
+
     public UserNotUpdatedException(String message) {
         super(message);
+    }
+
+    public UserNotUpdatedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserNotUpdatedException(UserNotUpdatedException cause) {
+        super(cause);
+    }
+
+    protected UserNotUpdatedException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
