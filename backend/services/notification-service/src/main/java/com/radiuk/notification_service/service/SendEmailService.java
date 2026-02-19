3482078e5e24176d84e951ea2b57699b850c321e
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2026-02-19 19:23:03 +0300

    rewrite SendEmailService

--- backend/services/notification-service/src/main/java/com/radiuk/notification_service/service/SendEmailService.java
+++ backend/services/notification-service/src/main/java/com/radiuk/notification_service/service/SendEmailService.java
@@ -1,9 +1,14 @@
 package com.radiuk.notification_service.service;
 
+import com.radiuk.notification_service.entity.EmailStatus;
 import com.radiuk.notification_service.entity.PasswordResetMessage;
 import com.radiuk.notification_service.repository.PasswordResetMessageRepository;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,7 +21,11 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SendEmailService {
 
+    private final JavaMailSender mailSender;
     private final PasswordResetMessageRepository repository;
+
+    private static final String SUBJECT = "Reset Password";
+    private static final String FRONTEND_URL = "http://localhost:3000/";
 
     @Transactional
     @Scheduled(fixedDelay = 60000)
@@ -24,8 +33,38 @@ public class SendEmailService {
         List<PasswordResetMessage> unsendMessages = repository.findBySentAtIsNull();
 
         for (PasswordResetMessage message : unsendMessages) {
-            log.info("Sending message to reset password email={}",  message.getEmailAddress());
-            message.setSentAt(Instant.now());
+            message.setStatus(EmailStatus.PENDING);
+            try {
+                sendEmail(message);
+                message.setSentAt(Instant.now());
+                message.setStatus(EmailStatus.SENT);
+                log.info("Email successfully sent to {}", message.getEmailAddress());
+            } catch (Exception exception) {
+                message.setStatus(EmailStatus.FAILED);
+                log.error("Failed to send email to {}", message.getEmailAddress(), exception);
+            }
         }
+    }
+
+    private void sendEmail(PasswordResetMessage message) throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper =
+                new MimeMessageHelper(mimeMessage, false, "UTF-8");
+
+        helper.setTo(message.getEmailAddress());
+        helper.setSubject(SUBJECT);
+
+        String resetLink = FRONTEND_URL + "reset?token=" + message.getToken();
+
+        String html = """
+            Dear user,<br><br>
+            Click the link below to reset your password:<br><br>
+            %s - Reset Password<br><br>
+            If you did not request this, ignore this email.
+            """.formatted(resetLink);
+
+        helper.setText(html, true);
+
+        mailSender.send(mimeMessage);
     }
 }
